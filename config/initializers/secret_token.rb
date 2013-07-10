b1cf9905dc0d14965e428ab14aea7807aec090b8
Sai Sandeep <sunnyisurs@gmail.com>
2013-07-11 02:14:55 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mappey::Application.config.secret_token = '673bae1da9e52e3662aed00de3ecb86c9707110b7488e83c06f2c34f51d0878fa78fa5dc78e50f5b5c921bb0a49383d0e93699bc563a668064c73a2e901fe351'
